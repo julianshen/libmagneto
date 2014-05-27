58b0b1411cc4899469475b7f478b4e2bc9c98296
Julian Shen <julianshen@gmail.com>
2014-05-27 17:54:41 +0800

    initial release

---
+++ src/main/java/org/mj/libmagneto/MagnetURI.java
@@ -0,0 +1,104 @@
+package org.mj.libmagneto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.net.MalformedURLException;
+import java.net.URLDecoder;
+
+
+public class MagnetURI {
+   public Part dn = null;
+   public Part xt = null;
+   public int xl = -1;
+   public Part as = null;
+   public Part xs = null;
+   public Part mt = null;
+   public Part tr = null;
+
+   protected static class Part {
+       private List<String> _values = new ArrayList<String>();
+
+       public String value() {
+           if(_values.size() == 0) {
+               return null;
+           }
+
+           return _values.get(0);
+       }
+
+       public String[] values() {
+           String[] result = new String[_values.size()];
+
+           _values.toArray(result);
+           return result;
+       }
+
+       public void addValue(String val) {
+           _values.add(val);
+       }
+   }
+
+   public static MagnetURI parse(String uriStr) throws MalformedURLException {
+       if(!uriStr.startsWith("magnet:?")) {
+           throw new MalformedURLException("Wrong scheme.");
+       }
+
+       MagnetURI magnetUri = new MagnetURI();
+
+       Map<String, Part> parts = new HashMap<String, Part>();
+       String queryStr = uriStr.substring(8);
+
+       String[] queries = queryStr.split("&");
+       for(String q:queries) {
+           String[] v = q.split("=");
+           String key = v[0];
+           String val = v[1];
+
+           if(key.length() > 2 && key.charAt('2') != '.') {
+               continue;
+           }
+
+           key = key.substring(0,2);
+
+           if("xl".equals(key)) {
+               try {
+                    magnetUri.xl = Integer.parseInt(val);
+               } catch(NumberFormatException e) {
+               }
+           } else {
+               Part part = parts.get(key);
+
+               if(part == null) {
+                   part = new Part();
+                   parts.put(key, part);
+               }
+               part.addValue(URLDecoder.decode(val));
+           }
+       }
+
+       magnetUri.dn = parts.get("dn");
+       magnetUri.dn = assignDummyIfNull(magnetUri.dn);
+       magnetUri.xt = parts.get("xt");
+       magnetUri.xt = assignDummyIfNull(magnetUri.xt);
+       magnetUri.as = parts.get("as");
+       magnetUri.as = assignDummyIfNull(magnetUri.as);
+       magnetUri.xs = parts.get("xs");
+       magnetUri.xs = assignDummyIfNull(magnetUri.xs);
+       magnetUri.mt = parts.get("mt");
+       magnetUri.mt = assignDummyIfNull(magnetUri.mt);
+       magnetUri.tr = parts.get("tr");
+       magnetUri.tr = assignDummyIfNull(magnetUri.tr);
+
+       return magnetUri;
+   }
+
+   private static Part assignDummyIfNull(Part p) {
+     if(p == null) {
+       return new Part();
+     }
+
+     return p;
+   }
+}
---
+++ src/test/java/org/mj/libmagneto/TestMagnetURI.java
@@ -0,0 +1,39 @@
+package org.mj.libmagneto;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
+
+public class TestMagnetURI {
+    public static final String TEST_URL_1 = "magnet:?xt=urn:btih:4QW4F2DHMB7BMCHIXOTPLDQP3IAW2BZW&dn=Rookie.Blue.S05E02.HDTV.x264-KILLERS&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.publicbt.com:80&tr=udp://tracker.istole.it:80&tr=udp://open.demonii.com:80&tr=udp://tracker.coppersurfer.tk:80";
+
+    @Test
+    public void checkBitTorrentURN() {
+        try {
+            MagnetURI muri = MagnetURI.parse(TEST_URL_1);
+
+            String[] xt = muri.xt.values();
+            org.junit.Assert.assertThat(Arrays.asList(xt), hasItem(containsString("urn:btih:"))); 
+
+        } catch(Exception e) {
+            org.junit.Assert.fail(e.toString());
+        }
+    }
+
+    @Test
+    public void checkTrackers() {
+        try {
+            MagnetURI muri = MagnetURI.parse(TEST_URL_1);
+            String[] trackers = muri.tr.values();
+            
+            for(String tracker:trackers) {
+                 new java.net.URI(tracker);
+            }
+        } catch(Exception e) {
+            org.junit.Assert.fail(e.toString());
+        }
+    }
+
+}
